5e877cb880f1cf6c474dfe5892062d6cc698566c
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-13 03:00:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,4 +64,13 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10 
+    GROUP BY students.id
+    SQL
+  end
 end
