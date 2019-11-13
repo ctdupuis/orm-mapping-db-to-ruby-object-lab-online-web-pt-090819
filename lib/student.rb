51dc70256f771d346371398e9a3f4fb0958b2f0d
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-13 02:53:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = "SELECT * FROM students WHERE grade = 9"
     DB[:conn].execute(sql)
   end
