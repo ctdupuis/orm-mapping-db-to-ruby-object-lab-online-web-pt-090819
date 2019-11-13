4afedb4eee77e2d2bfe8fa86128a4ca9f0270d1f
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-13 02:49:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,10 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM songs WHERE name = ? LIMIT 1 
+    SELECT *
+    FROM songs
+    WHERE name = ?
+    LIMIT 1 
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
